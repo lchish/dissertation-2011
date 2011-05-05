19b98097bcc0598d4848466785a6cbbd6af30f0c
lchish <lchish@51dc9503-e6af-f276-0477-6bec1050498a>
2011-05-05 08:26:30 +0000

    Making some large changes next commiting incase I need to rollback. This has the initial lighting set up and other stuff.

--- Viewer/Sun.java
+++ Viewer/Sun.java
@@ -4,12 +4,17 @@ import javax.media.opengl.glu.GLUquadric;
 
 
 public class Sun {
-	private float x,y,z;
+	public static float x,y,z;
+	public static float [] ambientLight = {0f,0f,0f,1f};
+	public static float [] diffuseLight = {1f,1f,1f,1f};
+	public static float [] specularLight = {1f,1f,1f,1f};
+	
 	private GLU glu = new GLU();
 	public Sun(float x,float y,float z){
-		this.x =x; this.y=y;this.z=z;
+		Sun.x =x; Sun.y=y;Sun.z=z;
 	}
 	public void draw(GL2 gl){
+		lighting(gl);
 		gl.glPushMatrix();
 		gl.glColor3f(1.0f, 1.0f, 1.0f);
 		GLUquadric quadratic = glu.gluNewQuadric();
@@ -17,9 +22,14 @@ public class Sun {
     	glu.gluQuadricNormals(quadratic,GLU.GLU_SMOOTH);
     	gl.glTranslatef(x,y,z);
 		glu.gluSphere(quadratic, 400f, 20, 20);
-		
 		gl.glPopMatrix();
-		
+	}
+	private void lighting(GL2 gl) {
+		float [] pos = {x,y+100f,z};
+		gl.glLightfv(GL2.GL_LIGHT0,GL2.GL_POSITION,pos,0);
+		gl.glLightfv(GL2.GL_LIGHT0,GL2.GL_AMBIENT,ambientLight,0);
+		gl.glLightfv(GL2.GL_LIGHT0,GL2.GL_DIFFUSE,diffuseLight,0);
+		gl.glLightfv(GL2.GL_LIGHT0,GL2.GL_SPECULAR,specularLight,0);
 	}
 	public void update(){
 		//move the xyz coordinates according to our time tick
