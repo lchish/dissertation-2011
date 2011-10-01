93eb2f2df5f552715dd8720ba9dbcfa9b3be5d78
lchish <lchish@51dc9503-e6af-f276-0477-6bec1050498a>
2011-10-01 02:41:59 +0000

    aggregator optimisation complete. Initial testing looks very good

--- src/Aggregator.java
+++ src/Aggregator.java
@@ -1,5 +1,4 @@
 import java.io.*;
-import java.util.Calendar;
 
 
 public class Aggregator {
@@ -8,7 +7,7 @@ public class Aggregator {
 	private Time time;
 	public long [][] totalData;
 	public boolean [][] stepData;
-	public long steps;//number of times the aggregator has been run
+	public long steps;//number of seconds the during the current run. ratio of sunlight hours is sunlightSeconds / steps
 	private boolean runOnce;
 	private static byte toSignedByte(int b){
 		int a = b;
@@ -21,29 +20,36 @@ public class Aggregator {
 		Time time = new Time(2011,9,1,17,0,0);
 		Aggregator a = new Aggregator(time,new DunedinMap("dunedin.txt","terrain.tga","suburbs.tga"),new Sun(time));
 		//a.runForYear("output.csv");
-		a.run();
+		//a.run();
+		a.runOptimised();
 		a.writeTga(a.stepData,"aggregator.tga");
 		//a.writeCSV(a.stepData,"out.csv");
 	}
 	public void run(){
 		if(time.getTimeSpeed()!=0  && sun.getElevation() > 0.0|| !runOnce){
 			runOnce = true;
-			for(int x = 0;x<dunedinMap.MapWidth;x++){
-				for(int y=0;y<dunedinMap.MapHeight;y++){
-					if(!dunedinMap.inWater(x, y)){
-						if(!rayLighting(x, y)){//if not in shadow
-							stepData[x][y] = false;
-							totalData[x][y]++;
-						}else{
-							stepData[x][y] = true;
-						}
-					}
-				}
-			}		
+			
+			rayLightingNonOptimised(0, 0, dunedinMap.getWidth(), dunedinMap.getHeight());
 			steps+=time.getTimeSpeed();
 		}
 	}
 
+	public void runOptimised(){
+		if(time.getTimeSpeed()!=0  && sun.getElevation() > 0.0|| !runOnce){
+			runOnce = true;
+			int stepSize  = 10,x,y;
+			for(x=0;x+stepSize<dunedinMap.getWidth();x+= stepSize){
+				for(y=0;y+stepSize<dunedinMap.getHeight();y+=stepSize){
+					rayLightingOptimised(x, y, stepSize, stepSize);
+				}
+				//finish off the final squares
+				//rayLightingNonOptimised(x, y, stepSize, dunedinMap.getHeight()-y);
+			}
+			//rayLightingNonOptimised(x, 0, dunedinMap.getWidth()-x,dunedinMap.getHeight());
+			steps+=time.getTimeSpeed();
+		}
+	}
+	
 	public void update(){//update the aggregator ONLY USE IF NOT USING THE VIEWERS UPDATE
 		time.update(sun);
 		sun.updateNew(time);
@@ -110,14 +116,21 @@ public class Aggregator {
 
 			for(int y=dunedinMap.MapHeight-1;y>=0;y--){
 				for(int x = 0;x<dunedinMap.MapWidth;x++){
-					int tmp = stepData[x][y] ? 0: 255;
-					byte []buf2 = new byte[3];
-					buf2[0] = toSignedByte(tmp);
-					buf2[1] = toSignedByte(tmp);
-					buf2[2] = toSignedByte(tmp);
-					bis.write(buf2);
+					if(dunedinMap.inWater(x, y)){
+						byte []buf2 = new byte[3];
+						buf2[0] = toSignedByte(0xff);
+						buf2[1] = toSignedByte(0);
+						buf2[2] = toSignedByte(0);
+						bis.write(buf2);
+					}else{
+						int tmp = stepData[x][y] ? 0: 255;
+						byte []buf2 = new byte[3];
+						buf2[0] = toSignedByte(tmp);
+						buf2[1] = toSignedByte(tmp);
+						buf2[2] = toSignedByte(tmp);
+						bis.write(buf2);
+					}
 				}
-				System.out.println();
 			}
 		}catch(Exception e){
 			e.printStackTrace();
@@ -149,14 +162,21 @@ public class Aggregator {
 
 			for(int y=dunedinMap.MapHeight-1;y>=0;y--){
 				for(int x = 0;x<dunedinMap.MapWidth;x++){
-					int tmp = (int) (totalData[x][y] /steps);
-					byte []buf2 = new byte[3];
-					buf2[0] = toSignedByte(tmp);
-					buf2[1] = toSignedByte(tmp);
-					buf2[2] = toSignedByte(tmp);
-					bis.write(buf2);
+					if(dunedinMap.inWater(x, y)){
+						byte []buf2 = new byte[3];
+						buf2[0] = toSignedByte(0);
+						buf2[1] = toSignedByte(0);
+						buf2[2] = toSignedByte(0xff);
+						bis.write(buf2);
+					}else{
+						int tmp = (int) (totalData[x][y] /steps);
+						byte []buf2 = new byte[3];
+						buf2[0] = toSignedByte(tmp);
+						buf2[1] = toSignedByte(tmp);
+						buf2[2] = toSignedByte(tmp);
+						bis.write(buf2);
+					}
 				}
-				System.out.println();
 			}
 		}catch(Exception e){
 			e.printStackTrace();
@@ -179,14 +199,79 @@ public class Aggregator {
 		runOnce = false;
 	}
 
+	private void rayLightingNonOptimised(int x,int z, int width,int height){
+		for(int xOff = x;xOff < x+width;xOff++){
+			for(int zOff = z; zOff < z+height;zOff++){
+				if(!dunedinMap.inWater(xOff, zOff) && dunedinMap.inSuburb(xOff, zOff)){
+					if(!rayLighting(xOff, zOff)){//if not in shadow
+						stepData[xOff][zOff] = false;
+						totalData[xOff][zOff]++;
+					}else{
+						stepData[xOff][zOff] = true;
+					}
+				}
+			}
+		}
+	}
+	
+	private void  rayLightingOptimised(int x,int z, int width,int height) {
+		//scan around the edges of the area
+		int xOff,zOff;
+		for(xOff = x,zOff=z; xOff < x+width;xOff++){//bottom left to bottom right
+				if(!rayLighting(xOff, zOff)){//if not in shadow
+					stepData[xOff][zOff] = false;
+					totalData[xOff][zOff]++;
+				}else{
+					stepData[xOff][zOff] = true;
+					rayLightingNonOptimised(x, z, width, height);
+					return;
+				}
+		}
+		for(xOff = x,zOff = z+height-1; xOff < x+width;xOff++){//top left to top right
+				if(!rayLighting(xOff, zOff)){//if not in shadow
+					stepData[xOff][zOff] = false;
+					totalData[xOff][zOff]++;
+				}else{
+					stepData[xOff][zOff] = true;
+					rayLightingNonOptimised(x, z, width, height);
+					return;
+				}
+		}
+		for( zOff = z,xOff = x; zOff < z+height;zOff++){//bottom left to top left
+				if(!rayLighting(xOff, zOff)){//if not in shadow
+					stepData[xOff][zOff] = false;
+					totalData[xOff][zOff]++;
+				}else{
+					stepData[xOff][zOff] = true;
+					rayLightingNonOptimised(x, z, width, height);
+					return;
+				}
+		}
+		for(xOff = x+width-1, zOff = z; zOff < z+height;zOff++){//bottom right to top right
+				if(!rayLighting(xOff, zOff)){//if not in shadow
+					stepData[xOff][zOff] = false;
+					totalData[xOff][zOff]++;
+				}else{
+					stepData[xOff][zOff] = true;
+					rayLightingNonOptimised(x, z, width, height);
+					return;
+				}
+		}
+		//woohoo now to assume that there are no shadows whithin the region we will fill in the remainder
+		for( xOff = x+1;xOff < width-1;xOff++){
+			for( zOff = z+1; zOff < height-1;zOff++){
+				stepData[xOff][zOff] =false;
+				totalData[xOff][zOff]++;
+			}
+		}
+	}
+	
 	public boolean rayLighting(int x,int z){
-		//System.out.println(x + " xz " + z);
 		Vec3f direction= sun.getDirectionUnitVector();
 		double initialPointHeight = dunedinMap.getMapHeight(x, z);
 		float tdx=1/direction.x,tdz=1/direction.z;//get the ball rolling
 		int xPos=x,zPos=z;
 		while(xPos <dunedinMap.getWidth()&&xPos>=0 && zPos<dunedinMap.getHeight() && zPos >=0){
-			//System.out.println(tdx+ " "+tdz);
 			if(heightTest(xPos,zPos,direction,Math.abs(tdx>tdz?tdx:tdz),initialPointHeight))
 				return true;
 			if(Math.abs(tdx)<Math.abs(tdz)){// a move in x is less than a move in z
